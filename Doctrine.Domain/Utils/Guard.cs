3af27c4ffdfaa5aea177466d29a6be7f0d5a0097
Yura95st <yura95st@gmail.com>
2015-02-07 20:10:09 +0200

    Add Guard utility class.

---
+++ Doctrine.Domain/Utils/Guard.cs
@@ -0,0 +1,50 @@
+﻿namespace Doctrine.Domain.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    ///     A static helper class that includes various parameter checking routines.
+    /// </summary>
+    public static class Guard
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Throws <see cref="ArgumentNullException" /> if the given argument is null.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The value is null.</exception>
+        /// <param name="argumentValue">The argument value to test.</param>
+        /// <param name="argumentName">The name of the argument to test.</param>
+        public static void NotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        /// <summary>
+        ///     Throws an exception if the tested string argument is null or an empty string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The string value is null.</exception>
+        /// <exception cref="ArgumentException">The string is empty.</exception>
+        /// <param name="argumentValue">The argument value to test.</param>
+        /// <param name="argumentName">The name of the argument to test.</param>
+        public static void NotNullOrEmpty(string argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            if (argumentValue.Length == 0)
+            {
+                throw new ArgumentException("The argument cannot be empty.", argumentName);
+            }
+        }
+
+        #endregion
+    }
+}
