5ec190c867ca72d51c4be96619bb2620d3b71f4e
Yura95st <yura95st@gmail.com>
2015-02-08 19:27:00 +0200

    Add new methods to the IUserService interface.

--- Doctrine.Domain/Services/Abstract/IUserService.cs
+++ Doctrine.Domain/Services/Abstract/IUserService.cs
@@ -1,6 +1,29 @@
 ﻿namespace Doctrine.Domain.Services.Abstract
 {
+    using Doctrine.Domain.Models;
+
     public interface IUserService
     {
+        /// <summary>Authenticates the user.</summary>
+        /// <param name="email">The email.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>Authenticated user.</returns>
+        User Authenticate(string email, string password);
+
+        /// <summary>Creates new user.</summary>
+        /// <param name="email">The email.</param>
+        /// <param name="fullName">The full name.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>Created user.</returns>
+        User Create(string email, string fullName, string password);
+
+        /// <summary>Deletes the user.</summary>
+        /// <param name="userId">The user identifier.</param>
+        void Delete(int userId);
+
+        /// <summary>Gets the by identifier.</summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns>User.</returns>
+        User GetById(int userId);
     }
 }
--- Doctrine.Domain/Services/Concrete/UserService.cs
+++ Doctrine.Domain/Services/Concrete/UserService.cs
@@ -1,6 +1,7 @@
 ﻿namespace Doctrine.Domain.Services.Concrete
 {
     using Doctrine.Domain.Dal;
+    using Doctrine.Domain.Models;
     using Doctrine.Domain.Services.Abstract;
     using Doctrine.Domain.Services.Common;
 
@@ -9,6 +10,26 @@
         public UserService(IUnitOfWork unitOfWork)
         : base(unitOfWork)
         {
+        }
+
+        public User Authenticate(string email, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User Create(string email, string fullName, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetById(int userId)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
