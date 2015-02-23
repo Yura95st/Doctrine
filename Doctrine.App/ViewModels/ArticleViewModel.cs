8f96089aaba94b48a3e2e089b7b0ea5fd91b7f34
Yura95st <yura95st@gmail.com>
2015-02-23 19:17:20 +0200

    Add ViewModel and View for Article and Comment.

---
+++ Doctrine.App/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,62 @@
+﻿namespace Doctrine.App.ViewModels
+{
+    using System.Collections.Generic;
+
+    using Doctrine.Domain.Models;
+
+    public class ArticleViewModel
+    {
+        public ArticleViewModel()
+        {
+            this.Comments = new List<CommentViewModel>();
+        }
+
+        public Article Article
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<CommentViewModel> Comments
+        {
+            get;
+            set;
+        }
+
+        public int CommentsCount
+        {
+            get;
+            set;
+        }
+
+        public int FavsCount
+        {
+            get;
+            set;
+        }
+
+        public bool IsPreviewMode
+        {
+            get;
+            set;
+        }
+
+        public int NegativeVotesCount
+        {
+            get;
+            set;
+        }
+
+        public int PositiveVotesCount
+        {
+            get;
+            set;
+        }
+
+        public int ViewsCount
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Doctrine.App/ViewModels/CommentViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace Doctrine.App.ViewModels
+{
+    using System.Collections.Generic;
+
+    using Doctrine.Domain.Models;
+
+    public class CommentViewModel
+    {
+        public CommentViewModel()
+        {
+            this.Replies = new List<CommentViewModel>();
+        }
+
+        public Comment Comment
+        {
+            get;
+            set;
+        }
+
+        public bool IsFromArticleAuthor
+        {
+            get;
+            set;
+        }
+
+        public int NegativeVotesCount
+        {
+            get;
+            set;
+        }
+
+        public int ParentCommentId
+        {
+            get;
+            set;
+        }
+
+        public int PositiveVotesCount
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<CommentViewModel> Replies
+        {
+            get;
+            set;
+        }
+    }
+}
